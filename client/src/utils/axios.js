4c58d8e550d85edf5785578c190573098ee42c96
Sandipan Malick <malicksandipan44@gmail.com>
2025-09-07 23:53:06 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5080",
+  baseURL: "https://sundarban-development-internship-project.onrender.com",
   withCredentials: true, // always send cookies
 });
 
