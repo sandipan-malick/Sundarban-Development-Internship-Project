530d8aa29fcd01fb1164f16b20847cb99d67f04f
Sandipan Malick <malicksandipan44@gmail.com>
2025-09-07 23:47:58 +0530

    Update EmailSendOtp.jsx

--- client/src/pages/EmailSendOtp.jsx
+++ client/src/pages/EmailSendOtp.jsx
@@ -15,7 +15,7 @@ function EmailSendOtp() {
   useEffect(() => {
     const sendOtp = async () => {
       try {
-        const res = await axios.post('http://localhost:5080/api/user/send-otp', {
+        const res = await axios.post('https://sundarban-development-internship-project.onrender.com/api/user/send-otp', {
           email: userData.email,
         });
 
