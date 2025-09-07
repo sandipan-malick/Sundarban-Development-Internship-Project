987ebcc528bac4472a1af096345dcbc3ca5aef9d
Sandipan Malick <malicksandipan44@gmail.com>
2025-09-08 01:17:35 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -36,8 +36,14 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
-    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, 
   })
 );
 
