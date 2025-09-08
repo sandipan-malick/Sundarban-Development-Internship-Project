b994f38ae11afa983bc6195fee3a9e30690db898
Sandipan Malick <malicksandipan44@gmail.com>
2025-09-08 12:20:10 +0530

    Update adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -71,7 +71,7 @@ exports.loginAdmin = async (req, res) => {
     res.cookie("adminToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
