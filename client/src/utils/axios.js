718994b31603071e18b47d2ad40cb6f8ab014aeb
Sandipan Malick <malicksandipan44@gmail.com>
2025-09-10 04:27:56 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,17 +1,18 @@
 // src/utils/axios.js
 import axios from "axios";
 
+// Create axios instance
 const api = axios.create({
   baseURL: "https://sundarban-development-internship-project.onrender.com",
   withCredentials: true, // always send cookies
 });
 
-// optional: auto-redirect on 401
+// Optional: auto-redirect on 401 (unauthorized)
 api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      window.location.href = "/login";
+      window.location.href = "/login"; // force redirect to login
     }
     return Promise.reject(err);
   }
