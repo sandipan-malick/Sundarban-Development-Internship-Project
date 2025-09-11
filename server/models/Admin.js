f2432fd5fb92e0237807e17e1e661947db4deea7
Sandipan Malick <malicksandipan44@gmail.com>
2025-09-12 00:48:23 +0530

    Update Admin.js

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // store hashed password
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Admin", adminSchema);
